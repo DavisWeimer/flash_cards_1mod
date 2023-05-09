48e834e82ffc3817f631a0d29679a7c850022087
Davis Weimer <davisweimer@gmail.com>
2023-05-09 13:44:26 -0600

    Create Card Class

--- lib/card.rb
+++ lib/card.rb
@@ -1,1 +1,9 @@
-lolol
+class Card
+    attr_reader :question, :answer, :category
+    def initialize(question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+
+end
